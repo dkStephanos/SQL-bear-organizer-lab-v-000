916a789d793a75ce63e8f971967d4bd443105b9d
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-13 16:37:01 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,16 @@
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Mr. Chocolate", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Rowdy", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Tabitha", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Sergeant Brown", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Melissa", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Grinch", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES ("Wendy", 11, "M", "Brown", "Angry", true);
-INSERT INTO students (name, grade, gpa, tardies)
+INSERT INTO bears (name, grade, gpa, tardies)
     VALUES (NULL, 11, "M", "Brown", "Angry", true);
